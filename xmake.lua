1f8fb208cb85140444315ef4a66a4874b84e84c1
Jean <jean.letessier@protonmail.com>
2024-06-16 20:44:47 +0200

    WebGPU instance creation.

---
+++ xmake.lua
@@ -0,0 +1,27 @@
+set_xmakever("2.8.1")
+
+set_project("LearnWebGPU")
+set_version("1.0.0")
+
+set_allowedplats("windows")
+set_allowedarchs("windows|x64")
+
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.vsxmake.autoupdate")
+set_languages("cxx20")
+set_optimize("fastest")
+
+add_requires("glfw", "wgpu-native")
+
+local outputdir = "$(mode)-$(os)-$(arch)"
+
+target("LearnWebGPU")
+    set_kind("binary")
+
+    set_targetdir("build/" .. outputdir .. "/LearnWebGPU/bin")
+    set_objectdir("build/" .. outputdir .. "/LearnWebGPU/obj")
+
+    -- C++ source files
+    add_files("LearnWebGPU/Source/**.cpp")
+    
+    add_packages("glfw", "wgpu-native")
